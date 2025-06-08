b4ffe9414adf771df5de49f7c8fc827eb149a1b0
kobusime <hiroki.71093971@icloud.com>
2025-06-08 11:38:39 +0900

    Add missing WebGL build files

---
+++ Build/shooting_game-.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50e4b87d59d5c226cc589aacecbd7f9e3141ed3f3df60b515115b4536f325e74
+size 462284
---
+++ Build/shooting_game-.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:246a3198310cbf75fd28ec68da8146dcb1909032684497a3443e63c2f7e79801
+size 26416
